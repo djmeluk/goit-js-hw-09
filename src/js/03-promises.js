390f5db9491ae06d8c48ac124be4d5dd126618a2
Oleh Tomashevskiy <kulemjd@ukr.net>
2023-01-23 19:35:47 +0200

    task 03 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+
+form.addEventListener('submit', onSubmit);
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  }); 
+  
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  let delayInput = +delay.value;
+  let stepInput = +step.value;
+    let amountInput = +amount.value;
+
+  for (let position = 1; position <= amountInput; position += 1) {
+    createPromise(position, delayInput)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
+    delayInput += stepInput;
   }
 }
